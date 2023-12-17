8e664e4268ce970b5cf11bdc677f81586f7196fe
root <root@ip-172-31-10-168.ap-northeast-1.compute.internal>
2023-12-17 21:44:31 +0900

    unzipファイルの修正

--- unzip_files.sh
+++ unzip_files.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 for file in ./tmp/*.zip
-# for file in ./tmp/evaluation.zip
 do
-  # UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE unzip -d ./data/ "$file"
-  UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE unzip -d ./data/ "$file"
+  filename=$(basename "$file")
+  if [ "$filename" = "Rotary_beacon_light.zip" ]; then
+    # Rotary_beacon_light.zip ファイルのみ特定のディレクトリに解凍
+    UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE unzip -d ./data/input/Rotary_beacon_light/ "$file"
+  else
+    # その他のファイルは元のディレクトリに解凍
+    UNZIP_DISABLE_ZIPBOMB_DETECTION=TRUE unzip -d ./data/input/ "$file"
+  fi
 done
